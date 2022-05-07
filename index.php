ebf261af9c960c9d21a291b51e47ac621f033c95
Ignea Ventus <grendless@gmail.com>
2022-05-07 23:29:27 +0200

    Second step. CSS seems to be done.

--- index.php
+++ index.php
@@ -3,30 +3,30 @@
 
     switch ($request_uri[0]) {
         case "/Portfolio":
-            loadsite("views/portfolio.php");
+            loadsite("./views/portfolio.php");
             break;
         case "/Contact":
         case "/Kontakt":
-            loadsite("views/contact.html");
+            loadsite("./views/contact.html");
             break;
         case "/mail":
             break;
         case "/service":
-            if ($request_uri[1]=="q=mail") require("modules/redirect.php");
+            if ($request_uri[1]=="q=mail") include("./modules/redirect.php");
             break;
         case "/":
         case "/About":
         case "/Omnie":
         case "/O mnie":
         default :
-            loadsite("views/aboutme.html");
+            loadsite("./views/aboutme.html");
             break;
     }
 
     function loadsite($addr) {
-        $site = require("modules/navbar.html");
-        $site += require($addr);
-        $site += require("modules/footer.html");
+        $site = include("./modules/navbar.html");
+        $site += include($addr);
+        $site += include("./modules/footer.html");
         return $site;
     }
 ?>
